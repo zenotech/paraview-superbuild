738b5b234756328ee27db3df8ffe8c5a37bacfa4
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-01-29 12:52:31 -0500

    MPICH cannot support parallel builds.

--- CMake/mpich2.cmake
+++ CMake/mpich2.cmake
@@ -4,8 +4,10 @@ add_external_project(
                     --prefix=<INSTALL_DIR>
                     --enable-shared
                     --disable-static
-                    --disable-cxx
                     --disable-f77
                     --disable-fc
+  # PVExternalProject_Add sets up an parallel build, by default.
+  # that doesn't work for the verion of MPICH2 we're using.
+  BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
   BUILD_IN_SOURCE 1
 )
