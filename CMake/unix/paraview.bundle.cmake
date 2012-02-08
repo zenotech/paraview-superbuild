3ba35f80ed010450c2c49a08713d84ed705e3852
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-02-08 10:58:52 -0500

    Update version numbers for 3.14.0-RC1

--- CMake/unix/paraview.bundle.cmake
+++ CMake/unix/paraview.bundle.cmake
@@ -1,7 +1,7 @@
 # script to "bundle" paraview.
 
 # install all ParaView's shared libraries.
-install(DIRECTORY "@install_location@/lib/paraview-3.12"
+install(DIRECTORY "@install_location@/lib/paraview-3.14"
   DESTINATION "lib"
   USE_SOURCE_PERMISSIONS
   COMPONENT superbuild)
@@ -9,12 +9,12 @@ install(DIRECTORY "@install_location@/lib/paraview-3.12"
 # install python
 if (ENABLE_PYTHON)
   install(DIRECTORY "@install_location@/lib/python2.7"
-    DESTINATION "lib/paraview-3.12/lib"
+    DESTINATION "lib/paraview-3.14/lib"
     USE_SOURCE_PERMISSIONS
     COMPONENT superbuild)
   # install pyconfig.h
   install (DIRECTORY "@install_location@/include/python2.7"
-    DESTINATION "lib/paraview-3.12/include"
+    DESTINATION "lib/paraview-3.14/include"
     USE_SOURCE_PERMISSIONS
     COMPONENT superbuild
     PATTERN "pyconfig.h")
@@ -24,9 +24,9 @@ endif()
 install(CODE
   "execute_process(COMMAND
     ${CMAKE_COMMAND}
-      -Dexecutable:PATH=${install_location}/lib/paraview-3.12/paraview
+      -Dexecutable:PATH=${install_location}/lib/paraview-3.14/paraview
       -Ddependencies_root:PATH=${install_location}
-      -Dtarget_root:PATH=\${CMAKE_INSTALL_PREFIX}/lib/paraview-3.12
+      -Dtarget_root:PATH=\${CMAKE_INSTALL_PREFIX}/lib/paraview-3.14
       -P ${CMAKE_CURRENT_LIST_DIR}/install_dependencies.cmake)"
   COMPONENT superbuild)
 
@@ -36,10 +36,10 @@ install(DIRECTORY
     # FIXME: we can reconfigure Qt to be built with inbuilt sqllite support to 
     # avoid the need for plugins.
     "@install_location@/plugins/"
-  DESTINATION "lib/paraview-3.12"
+  DESTINATION "lib/paraview-3.14"
   COMPONENT superbuild
   PATTERN "*.a" EXCLUDE
-  PATTERN "paraview-3.12" EXCLUDE
+  PATTERN "paraview-3.14" EXCLUDE
   PATTERN "fontconfig" EXCLUDE
   PATTERN "*.jar" EXCLUDE
   PATTERN "*.debug.*" EXCLUDE
@@ -55,12 +55,12 @@ endforeach()
 
 if (ENABLE_MPICH2)
   install(PROGRAMS "@install_location@/bin/mpiexec.hydra"
-    DESTINATION "lib/paraview-3.12"
+    DESTINATION "lib/paraview-3.14"
     COMPONENT superbuild
     RENAME "mpiexec")
   foreach (hydra_exe hydra_nameserver hydra_persist hydra_pmi_proxy)
     install(PROGRAMS "@install_location@/bin/${hydra_exe}"
-      DESTINATION "lib/paraview-3.12"
+      DESTINATION "lib/paraview-3.14"
       COMPONENT superbuild)
   endforeach()
 endif()
@@ -72,6 +72,6 @@ SET(CPACK_PACKAGE_NAME "ParaView")
 SET(CPACK_PACKAGE_VENDOR "Kitware, Inc.")
 SET(CPACK_PACKAGE_VERSION_MAJOR 3)
 SET(CPACK_PACKAGE_VERSION_MINOR 14)
-SET(CPACK_PACKAGE_VERSION_PATCH 0)
+SET(CPACK_PACKAGE_VERSION_PATCH "0-RC1")
 SET(CPACK_COMPONENTS_ALL "superbuild")
 INCLUDE(CPack)
