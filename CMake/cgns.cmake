36200a581408cbc60f2a4d10c14b0410428a8e40
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-02-09 10:01:36 -0500

    Fixed issue with cgns.

--- CMake/cgns.cmake
+++ CMake/cgns.cmake
@@ -4,8 +4,10 @@ add_external_project(
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
                     --prefix=<INSTALL_DIR>
                     --enable-shared=all
-                    --with-hdf5=<INSTALL_DIR>
-                    --with-szip=<INSTALL_DIR>/lib/libsz.so
+                    # don't build with HDF5 support, 
+                    # as that seems broken.
+                    #--with-hdf5=<INSTALL_DIR>
+                    #--with-szip=<INSTALL_DIR>/lib/libsz.so
                     --with-zlib
                     --with-fortran=no
                     --enable-64bit
