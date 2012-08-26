b6938dfb0b5f5955cd033cb735dec7b309e0a626
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-08-26 14:35:49 -0400

    Fix hdf5 issues when using nmake or ninja.
    The problem was that hdf5 cmake is misconfigured to not detect its running MSVC
    when nmake or ninja files are used. Overcoming that with a patch. This solves a
    linker error we got with missing definition for gettimeofday.

--- Projects/hdf5.cmake
+++ Projects/hdf5.cmake
@@ -1,3 +1,4 @@
+
 add_external_project(
   hdf5
   DEPENDS zlib szip
@@ -8,3 +9,15 @@ add_external_project(
     -DHDF5_ENABLE_SZIP_ENCODING:BOOL=TRUE
     -DHDF5_BUILD_HL_LIB:BOOL=TRUE
 )
+
+if (MSVC)
+ # hdf5 has a bug with MSVC compiler where it doesn't realize its using MSVC
+ # compiler when using nmake or ninja generators. This patch fixes that.
+ add_external_project_step(patch_fix_msvc
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different
+          ${ParaViewSuperBuild_PROJECTS_DIR}/patches/hdf5.config.cmake.ConfigureChecks.cmake
+          <SOURCE_DIR>/config/cmake/ConfigureChecks.cmake
+  DEPENDEES update # do after update
+  DEPENDERS patch  # do before patch
+  )
+endif()
