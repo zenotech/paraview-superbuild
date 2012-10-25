9c2e8efd8477b9b6a94431a0a59b5337cc177386
George Zagaris <george.zagaris@kitware.com>
2012-10-25 11:45:38 -0400

    Serialize install cmds to avoid race conditions
    Change-Id: Ia7c7b3ffd839d340f7e06d54d3fc3a500085525a

--- Projects/install_cosmologytools.cmake
+++ Projects/install_cosmologytools.cmake
@@ -1,7 +1,15 @@
 
 execute_process(
     COMMAND ${CMAKE_COMMAND} -E make_directory ${TMP_DIR}/CosmologyToolsPlugin
+    WORKING_DIRECTORY ${TMP_DIR}
+)
+
+execute_process(
     COMMAND ${CMAKE_COMMAND} -E copy ${BINARY_DIR}/libs/libPVCosmologyToolsPlugins.so ${TMP_DIR}/CosmologyToolsPlugin
-    COMMAND ${CMAKE_COMMAND} -E tar czvf ${bundle_name} ${TMP_DIR}/CosmologyToolsPlugin
+    WORKING_DIRECTORY ${TMP_DIR}
+)
+
+execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar cvfz ${bundle_name} ${TMP_DIR}/CosmologyToolsPlugin
     WORKING_DIRECTORY ${TMP_DIR}
 )
