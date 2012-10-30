9d4e148b85648a304dd8238c7c57900d51e8b74e
George Zagaris <george.zagaris@kitware.com>
2012-10-30 13:40:52 -0400

    Explicitely tell DIY to use the bundled MPI
    Change-Id: Iee515bf368b7415f5bd05f0c462c352ee7d14694

--- Projects/diy.cmake
+++ Projects/diy.cmake
@@ -3,6 +3,9 @@
 add_external_project(diy
     DEPENDS mpi
     BUILD_IN_SOURCE 1
+    PROCESS_ENVIRONMENT
+      MPICC "<INSTALL_DIR>/bin/mpicc"
+      MPICXX "<INSTALL_DIR>/bin/mpicxx"
     CONFIGURE_COMMAND
         <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-fpic --disable-openmp
     BUILD_COMMAND make
