93733ff3f9aec2d0f4103b21073fc9e899595d5f
George Zagaris <george.zagaris@kitware.com>
2012-11-05 12:00:09 -0500

    Fix install rules for Mac
    Change-Id: Id8d5a11099c66c8668747b8dfb2597a4399b2e3f

--- Projects/install_cosmologytools.cmake
+++ Projects/install_cosmologytools.cmake
@@ -4,10 +4,19 @@ execute_process(
     WORKING_DIRECTORY ${TMP_DIR}
 )
 
-execute_process(
+if (UNIX AND NOT APPLE)
+  execute_process(
     COMMAND ${CMAKE_COMMAND} -E copy ${BINARY_DIR}/libs/libPVCosmologyToolsPlugins.so ${TMP_DIR}/CosmologyToolsPlugin
     WORKING_DIRECTORY ${TMP_DIR}
-)
+  )
+endif()
+
+if (APPLE)
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E copy ${BINARY_DIR}/libs/libPVCosmologyToolsPlugins.dylib ${TMP_DIR}/CosmologyToolsPlugin
+    WORKING_DIRECTORY ${TMP_DIR}
+  )
+endif()
 
 execute_process(
     COMMAND ${CMAKE_COMMAND} -E tar cvfz ${bundle_name} ${TMP_DIR}/CosmologyToolsPlugin
