3f5818d3a8b70542cad72872cc8fcb3b893dfd64
David C. Lonie <david.lonie@kitware.com>
2012-11-13 12:45:35 -0500

    Get matplotlib building on Ninja.
    The medea builds were failing due to having an uninterpreted
    ${CMAKE_COMMAND} in the build.ninja file. This patch interprets the var
    when using ninja and passes the var otherwise. This will still break for
    ninja builds with spaces in the path.
    
    Change-Id: If22ed0d6f1f3f6e6c62de5899697a189a690a138

--- Projects/matplotlib.cmake
+++ Projects/matplotlib.cmake
@@ -1,5 +1,17 @@
 set (_install_location "<INSTALL_DIR>")
 
+# BUILD_COMMAND can't handle spaces in the executable name. Worse, for
+# Makefile builds, it writes out a cmake script where ${CMAKE_COMMAND} can
+# be expanded, while on ninja builds it just adds a new build rule to
+# the build.ninja file, where ${CMAKE_COMMAND} cannot be interpreted. This
+# should work if either (a) A Makefile generator is used with or without
+# spaces in the CMake command path, or (b) Ninja is used with no spaces
+# in the path.
+set(SAFE_CMAKE_COMMAND "\\\${CMAKE_COMMAND}")
+if(${CMAKE_GENERATOR} STREQUAL "Ninja")
+  set(SAFE_CMAKE_COMMAND "${CMAKE_COMMAND}")
+endif()
+
 add_external_project(matplotlib
   DEPENDS python numpy png freetype
   PATCH_COMMAND
@@ -8,14 +20,14 @@ add_external_project(matplotlib
       "-DPATCH_OUTPUT_DIR:PATH=${CMAKE_BINARY_DIR}"
       "-DSOURCE_DIR:PATH=<SOURCE_DIR>"
       "-DINSTALL_DIR:PATH=<INSTALL_DIR>"
-      -P "${CMAKE_CURRENT_LIST_DIR}/matplotlib.patch.cmake"
+      -P "${SuperBuild_PROJECTS_DIR}/matplotlib.patch.cmake"
   CONFIGURE_COMMAND ""
   INSTALL_COMMAND ""
   BUILD_IN_SOURCE 1
   BUILD_COMMAND
-    \\\${CMAKE_COMMAND} -DPYTHON_EXECUTABLE:PATH=${pv_python_executable}
-                        -DMATPLOTLIB_SOURCE_DIR:PATH=<SOURCE_DIR>
-                        -DMATPLOTLIB_INSTALL_DIR:PATH=<INSTALL_DIR>
-                        -DNUMPY_INSTALL_DIR:PATH=<INSTALL_DIR>
-                        -P ${CMAKE_CURRENT_LIST_DIR}/matplotlib.build.cmake
+    "${SAFE_CMAKE_COMMAND}" -DPYTHON_EXECUTABLE:PATH=${pv_python_executable}
+                            -DMATPLOTLIB_SOURCE_DIR:PATH=<SOURCE_DIR>
+                            -DMATPLOTLIB_INSTALL_DIR:PATH=<INSTALL_DIR>
+                            -DNUMPY_INSTALL_DIR:PATH=<INSTALL_DIR>
+                            -P ${SuperBuild_PROJECTS_DIR}/matplotlib.build.cmake
 )
