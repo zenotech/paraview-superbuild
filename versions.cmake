38ed514e9291ceb55b292450245198dc53b7913e
George Zagaris <george.zagaris@kitware.com>
2012-12-13 10:06:45 -0500

    Move DIY to revision 155
    Change-Id: I721215258e7b23cc2b6cc4c9545b7d03267ff927

--- versions.cmake
+++ versions.cmake
@@ -101,7 +101,7 @@ add_revision(paraview
 if (TRUST_SVN_CERTIFICATES_AUTOMATICALLY)
   add_revision(diy
      SVN_REPOSITORY https://svn.mcs.anl.gov/repos/diy/trunk
-     SVN_REVISION -r144
+     SVN_REVISION -r155
      SVN_TRUST_CERT 1)
 else()
   add_revision(diy
