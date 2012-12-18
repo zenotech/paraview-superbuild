ae8327a6d631056d6c58f3a098c632ea8a7f763f
George Zagaris <george.zagaris@kitware.com>
2012-12-18 09:30:48 -0500

    Add logic for using system mpi with diy
    Change-Id: I489398f33436b89d6f8edf8d3f8461ae2bc1391e

--- Projects/diy.cmake
+++ Projects/diy.cmake
@@ -1,6 +1,19 @@
 ## Builds DIY library
 ## https://svn.mcs.anl.gov/repos/diy/trunk
-add_external_project(diy
+if(USE_SYSTEM_mpi)
+  add_external_project(diy
+    DEPENDS mpi hdf5
+    BUILD_IN_SOURCE 1
+    PROCESS_ENVIRONMENT
+      MPICC "${MPI_C_COMPILER}"
+      MPICXX "${MPI_CXX_COMPILER}"
+    CONFIGURE_COMMAND
+        <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-fpic --disable-openmp
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install
+    )
+else()
+  add_external_project(diy
     DEPENDS mpi hdf5
     BUILD_IN_SOURCE 1
     PROCESS_ENVIRONMENT
@@ -10,5 +23,6 @@ add_external_project(diy
         <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-fpic --disable-openmp
     BUILD_COMMAND make
     INSTALL_COMMAND make install
-        )
+    )
+endif()
 
