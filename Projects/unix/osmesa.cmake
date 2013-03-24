e48001e0e91f069614113036076dac5f8050b008
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-03-23 21:00:22 -0400

    Build shared OSMesa.
    Change-Id: I3190211fdf4ef1c70e035fc77fe5b8deace82e1d

--- Projects/unix/osmesa.cmake
+++ Projects/unix/osmesa.cmake
@@ -2,7 +2,8 @@ add_external_project(osmesa
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
                     --prefix=<INSTALL_DIR>
                     --disable-shared
-                    --enable-static
+                    --enable-shared
+                    --disable-static
                     --enable-osmesa
                     --without-x
                     --with-gallium-drivers=
@@ -15,8 +16,8 @@ add_external_project(osmesa
 add_extra_cmake_args(
   -DOPENGL_INCLUDE_DIR:PATH=<INSTALL_DIR>/include
   -DOPENGL_gl_LIBRARY:FILEPATH=
-  -DOPENGL_glu_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libGLU.a
+  -DOPENGL_glu_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libGLU.so
   -DVTK_OPENGL_HAS_OSMESA:BOOL=ON
   -DOSMESA_INCLUDE_DIR:PATH=<INSTALL_DIR>/include
-  -DOSMESA_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libOSMesa.a
+  -DOSMESA_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libOSMesa.so
   -DVTK_USE_X:BOOL=OFF)
