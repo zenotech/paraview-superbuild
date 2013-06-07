8557f3f09fedc8a953f61b99ecba6a90da2816ce
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-06-07 10:49:39 -0400

    Updating version to RC3
    Change-Id: I23c22deae54222f62187bbbe8e94711f23dde447

--- CMake/paraview_version.cmake
+++ CMake/paraview_version.cmake
@@ -3,6 +3,6 @@
 set (pv_version_major 4)
 set (pv_version_minor 0)
 set (pv_version_patch 0)
-set (pv_version_suffix RC2)
+set (pv_version_suffix RC3)
 set (pv_version "${pv_version_major}.${pv_version_minor}")
 set (pv_version_long "${pv_version}.${pv_version_patch}-${pv_version_suffix}")
