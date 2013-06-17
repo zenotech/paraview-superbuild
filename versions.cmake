516c90fd9468a97f9ff36aecc5dc223c70c88f9d
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-06-17 12:21:13 -0400

    Update dependency versions.
    Change-Id: I94aa88716336f3372f135257332a08e72909cb9e

--- versions.cmake
+++ versions.cmake
@@ -9,58 +9,58 @@
 # endif()
 
 add_revision(zlib
-  URL "http://www.paraview.org/files/v3.98/dependencies/zlib-1.2.7.tar.gz"
+  URL "http://www.paraview.org/files/dependencies/zlib-1.2.7.tar.gz"
   URL_MD5 60df6a37c56e7c1366cca812414f7b85)
 # NOTE: if updating zlib version, fix patch in zlib.cmake
 
 
 add_revision(png
-  URL "http://paraview.org/files/v3.98/dependencies/libpng-1.4.8.tar.gz"
+  URL "http://paraview.org/files/dependencies/libpng-1.4.8.tar.gz"
   URL_MD5 49c6e05be5fa88ed815945d7ca7d4aa9)
 
 add_revision(freetype
-  URL "http://paraview.org/files/v3.98/dependencies/freetype-2.4.8.tar.gz"
+  URL "http://paraview.org/files/dependencies/freetype-2.4.8.tar.gz"
   URL_MD5 "5d82aaa9a4abc0ebbd592783208d9c76")
 
 add_revision(szip
-  URL "http://paraview.org/files/v3.98/dependencies/szip-2.1.tar.gz"
+  URL "http://paraview.org/files/dependencies/szip-2.1.tar.gz"
   URL_MD5 902f831bcefb69c6b635374424acbead)
 
 add_revision(hdf5
-  URL http://paraview.org/files/v3.98/dependencies/hdf5-1.8.9.tar.gz
+  URL "http://paraview.org/files/dependencies/hdf5-1.8.9.tar.gz"
   URL_MD5 d1266bb7416ef089400a15cc7c963218)
 
 add_revision(silo
-  URL "http://paraview.org/files/v3.98/dependencies/silo-4.8-bsd.tar.gz"
+  URL "http://paraview.org/files/dependencies/silo-4.8-bsd.tar.gz"
   URL_MD5 d864e383f25b5b047b98aa2d5562d379)
 
 add_revision(cgns
-  URL "http://www.paraview.org/files/v3.98/dependencies/cgnslib_3.1.3-4.tar.gz"
+  URL "http://www.paraview.org/files/dependencies/cgnslib_3.1.3-4.tar.gz"
   URL_MD5 442bba32b576f3429cbd086af43fd4ae)
 
 add_revision(ffmpeg
-  URL "http://paraview.org/files/v3.98/dependencies/ffmpeg-0.6.5.tar.gz"
+  URL "http://paraview.org/files/dependencies/ffmpeg-0.6.5.tar.gz"
   URL_MD5 451054dae3b3d33a86c2c48bd12d56e7)
 
 add_revision(libxml2
-  URL "http://paraview.org/files/v3.98/dependencies/libxml2-2.7.8.tar.gz"
+  URL "http://paraview.org/files/dependencies/libxml2-2.7.8.tar.gz"
   URL_MD5 8127a65e8c3b08856093099b52599c86)
 
 add_revision(fontconfig
-  URL "http://paraview.org/files/v3.98/dependencies/fontconfig-2.8.0.tar.gz"
+  URL "http://paraview.org/files/dependencies/fontconfig-2.8.0.tar.gz"
   URL_MD5 77e15a92006ddc2adbb06f840d591c0e)
 
 add_revision(qt
-  URL "http://www.paraview.org/files/v3.98/dependencies/qt-everywhere-opensource-src-4.8.2.tar.gz"
+  URL "http://www.paraview.org/files/dependencies/qt-everywhere-opensource-src-4.8.2.tar.gz"
   URL_MD5 3c1146ddf56247e16782f96910a8423b)
 
 if (WIN32 OR (CROSS_BUILD_STAGE STREQUAL "CROSS"))
   add_revision(python
-    URL "http://www.paraview.org/files/v3.98/dependencies/Python-2.7.3.tgz"
+    URL "http://www.paraview.org/files/dependencies/Python-2.7.3.tgz"
     URL_MD5 "2cf641732ac23b18d139be077bd906cd")
 else()
   add_revision(python
-    URL "http://paraview.org/files/v3.98/dependencies/Python-2.7.2.tgz"
+    URL "http://paraview.org/files/dependencies/Python-2.7.2.tgz"
     URL_MD5 "0ddfe265f1b3d0a8c2459f5bf66894c7")
 endif()
 
@@ -69,43 +69,41 @@ add_revision(numpy
   URL_MD5 95ed6c9dcc94af1fc1642ea2a33c1bba)
 
 add_revision(matplotlib
-  URL "http://paraview.org/files/v3.98/dependencies/matplotlib-1.1.1_notests.tar.gz"
+  URL "http://paraview.org/files/dependencies/matplotlib-1.1.1_notests.tar.gz"
   URL_MD5 30ee59119599331bf1f3b6e838fee9a8)
 
 add_revision(boost
-  URL "http://www.paraview.org/files/v3.98/dependencies/boost_1_50_0.tar.gz"
+  URL "http://www.paraview.org/files/dependencies/boost_1_50_0.tar.gz"
   URL_MD5 dbc07ab0254df3dda6300fd737b3f264)
 
 add_revision(manta
-  URL "http://paraview.org/files/v3.98/dependencies/manta-r2439.tar.gz"
+  URL "http://paraview.org/files/dependencies/manta-r2439.tar.gz"
   URL_MD5 fbf4107fe2f6d7e8a5ae3dda71805bdc)
 
 if (UNIX)
   add_revision(mpi
-    URL "http://paraview.org/files/v3.98/dependencies/mpich2-1.4.1p1.tar.gz"
+    URL "http://paraview.org/files/dependencies/mpich2-1.4.1p1.tar.gz"
     URL_MD5 b470666749bcb4a0449a072a18e2c204)
 elseif (WIN32)
   add_revision(mpi
-    URL "http://www.paraview.org/files/v3.98/dependencies/openmpi-1.4.4.tar.gz"
+    URL "http://www.paraview.org/files/dependencies/openmpi-1.4.4.tar.gz"
     URL_MD5 7253c2a43445fbce2bf4f1dfbac113ad)
 endif()
 
 if (CROSS_BUILD_STAGE STREQUAL "CROSS")
   add_revision(mesa
-    URL ftp://ftp.freedesktop.org/pub/mesa/7.6.1/MesaLib-7.6.1.tar.gz
+    URL "http://www.paraview.org/files/dependencies/MesaLib-7.6.1.tar.gz"
     URL_MD5 e80fabad2e3eb7990adae773d6aeacba)
 else()
   add_revision(mesa
-    URL http://paraview.org/files/v3.98/dependencies/MesaLib-7.11.2.tar.gz
+    URL "http://paraview.org/files/dependencies/MesaLib-7.11.2.tar.gz"
     URL_MD5 b9e84efee3931c0acbccd1bb5a860554)
 endif()
 
 # We stick with 7.11.2 for Mesa version for now. Newer mesa doesn't seem to
 # build correctly with certain older compilers (e.g. on neser).
 add_revision(osmesa
-#    URL ftp://ftp.freedesktop.org/pub/mesa/8.0.5/MesaLib-8.0.5.tar.gz
-#    URL_MD5 cda5d101f43b8784fa60bdeaca4056f2)
-    URL http://paraview.org/files/v3.98/dependencies/MesaLib-7.11.2.tar.gz
+    URL "http://paraview.org/files/dependencies/MesaLib-7.11.2.tar.gz"
     URL_MD5 b9e84efee3931c0acbccd1bb5a860554)
 
 if (TRUST_SVN_CERTIFICATES_AUTOMATICALLY)
@@ -130,9 +128,9 @@ if (ParaView_FROM_GIT)
     GIT_TAG "master")
 else()
   # Variables to hold the URL and MD5 (optional)
-  set (ParaView_URL "" CACHE
+  set (ParaView_URL "http://www.paraview.org/files/v4.0/ParaView-v4.0.1-source.tgz" CACHE
     STRING "Specify the url for ParaView tarball")
-  set (ParaView_URL_MD5 "" CACHE
+  set (ParaView_URL_MD5 "6a65c4b03bd82393197f1311e6d9c750" CACHE
     STRING "MD5 of the ParaView tarball")
 
   # Get the length of the URL specified.
@@ -176,5 +174,5 @@ add_revision(nektar_plugin
   GIT_TAG master)
 
 add_revision(portfwd
-  URL http://iweb.dl.sourceforge.net/project/portfwd/portfwd/0.29/portfwd-0.29.tar.gz
+  URL "http://www.paraview.org/files/dependencies/portfwd-0.29.tar.gz"
   URL_MD5 93161c91e12b0d67ca52dc13708a2f2f)
