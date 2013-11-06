68a98642307474f5e5168752bcf77eebea8a73d0
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2013-11-06 08:58:42 -0700

    Add missing PVWeb server args
    Change-Id: I5bee0d40084f1590c9280b90500d4b3ba326c3a6

--- tests/basic_paraviewweb.py
+++ tests/basic_paraviewweb.py
@@ -10,11 +10,22 @@ class TestArgs:
     pass
 
 args = TestArgs()
+args.host    = 'localhost'
 args.port    = 8081
 args.debug   = 1
 args.timeout = 10
 args.content = ''
 args.nosignalhandlers = False
+args.authKey = 'vtkweb-secret'
+args.forceFlush = False
+args.testScriptPath = ''
+args.baselineImgDir = ''
+args.path = '.'
+args.file = None
+args.dsHost = None
+args.dsPort = 11111
+args.rsHost = None
+args.rsPort = 11111
 
 # Start server
 server.start_webserver(options=args, protocol=pv_web_visualizer._PipelineManager)
