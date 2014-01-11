23fab72ff302a9e4ace90786d46d5c8c3be46a6b
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-01-11 08:56:49 -0500

    Update version to 4.1.0
    Change-Id: I41aa135063613f5f9dae1545c33fe5d607249eb4

--- CMake/paraview_version.cmake
+++ CMake/paraview_version.cmake
@@ -3,7 +3,7 @@
 set (pv_version_major 4)
 set (pv_version_minor 1)
 set (pv_version_patch 0)
-set (pv_version_suffix "RC2")
+set (pv_version_suffix)
 set (pv_version "${pv_version_major}.${pv_version_minor}")
 if (pv_version_suffix)
   set (pv_version_long "${pv_version}.${pv_version_patch}-${pv_version_suffix}")
