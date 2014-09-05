12952b5e34b5324027a0156769591253f44a6cff
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-09-05 11:27:18 -0400

    Add support to pick clang toolchain on OsX.
    Change-Id: I9fada0bfabd44c9dedafe3d8b9be43d50baa26f3

--- Projects/boost.cmake
+++ Projects/boost.cmake
@@ -1,10 +1,17 @@
+set (extra_commands)
 
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  #set the platform to be clang if on apple and not gcc
+  set(extra_commands --with-toolset=clang)
+endif()
 add_external_project(boost
   DEPENDS zlib
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.sh --prefix=<INSTALL_DIR>
                               --with-libraries=date_time
+                              ${extra_commands}
   BUILD_COMMAND <SOURCE_DIR>/bjam
   INSTALL_COMMAND <SOURCE_DIR>/bjam --prefix=<INSTALL_DIR> install
 )
+unset(extra_commands)
