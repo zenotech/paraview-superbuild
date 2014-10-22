0e763a05ce0813f26021fa68d57081b93406ef24
Chuck Atkins <chuck.atkins@kitware.com>
2014-10-22 15:22:43 -0400

    Patch installation to remove absolute paths to the build dir
    Change-Id: I3a0fad2fdb0abbcd62d991a741620bdb71728669

--- Projects/unix/paraviewsdk.cmake
+++ Projects/unix/paraviewsdk.cmake
@@ -16,23 +16,32 @@ if (paraviewsdk_ENABLED)
   endif()
 
   # install all include files.
-  install(DIRECTORY "@install_location@/include/"
+  install(DIRECTORY "${install_location}/include/"
           DESTINATION "include"
           COMPONENT superbuild)
 
   # install all library files (including those for dependencies built).
-  install(DIRECTORY "@install_location@/lib/"
+  install(DIRECTORY "${install_location}/lib/"
           DESTINATION "lib"
           COMPONENT superbuild
           PATTERN "paraview-${pv_version}" EXCLUDE)
 
+  # Workaround to patch any hard-coded paths to the build folder
+  install(CODE "
+  file(GLOB_RECURSE ALL_CMAKE_FILES \"${install_location}/lib/cmake/*.cmake\")
+  foreach(F \${ALL_CMAKE_FILES})
+    execute_process(
+      COMMAND sed \"s|${install_location}|\${_IMPORT_PREFIX}|g\" -i \${F}
+    )
+  endforeach()")
+
   # install all CMake files.
-  install(DIRECTORY "@install_location@/lib/cmake/"
+  install(DIRECTORY "${install_location}/lib/cmake/"
           DESTINATION "lib/cmake"
           COMPONENT superbuild)
 
   # install all executables since these include the wrapping tools and others.
-  install(DIRECTORY "@install_location@/bin/"
+  install(DIRECTORY "${install_location}/bin/"
           DESTINATION "bin"
           USE_SOURCE_PERMISSIONS
           COMPONENT superbuild)
