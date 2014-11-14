06ef62d1436f49eed136736ce4208b5605d6fc86
Dan Lipsa <dan.lipsa@kitware.com>
2014-11-14 16:29:08 -0500

    Fix szip install.
    Change-Id: Ia358227df79d63aae55c05d91afa187a786a030b

--- Projects/szip.cmake
+++ Projects/szip.cmake
@@ -6,5 +6,15 @@ add_external_project(szip
 
 # any project depending on szip, inherits these cmake variables
 add_extra_cmake_args(
-    -DSZIP_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}sz${CMAKE_SHARED_LIBRARY_SUFFIX}
-    -DSZIP_INCLUDE_DIR:FILEPATH=<INSTALL_DIR>/include)
+  -DSZIP_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}sz${CMAKE_SHARED_LIBRARY_SUFFIX}
+  -DSZIP_INCLUDE_DIR:FILEPATH=<INSTALL_DIR>/include)
+
+# get_filename_component on line 10 is missing the 'component' (path).
+# this patch fixes that.
+add_external_project_step(patch_fix_install
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different
+  ${SuperBuild_PROJECTS_DIR}/patches/szip.config.cmake.SZIP-config.cmake.install.in
+  <SOURCE_DIR>/config/cmake/SZIP-config.cmake.install.in
+  DEPENDEES update # do after update
+  DEPENDERS patch  # do before patch
+  )
