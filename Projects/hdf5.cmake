9b875f82e3b3307e53f0d4d4c012ea9f28054136
Dan Lipsa <dan.lipsa@kitware.com>
2014-11-14 09:09:20 -0500

    Update hdf5
    Change-Id: Ifb13f331e673bb582c35c7cbf77406bd6448a38b

--- Projects/hdf5.cmake
+++ Projects/hdf5.cmake
@@ -3,11 +3,6 @@ add_external_project(
   hdf5
   DEPENDS zlib szip
 
-  # HDF5 1.8.9 has a CMake install rule bug. Fix that.
-  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${SuperBuild_PROJECTS_DIR}/patches/hdf5.CMakeLists.txt
-                <SOURCE_DIR>/CMakeLists.txt
-
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=TRUE
     -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=TRUE
@@ -17,15 +12,15 @@ add_external_project(
 )
 
 if (MSVC)
- # hdf5 has a bug with MSVC compiler where it doesn't realize its using MSVC
- # compiler when using nmake or ninja generators. This patch fixes that.
- add_external_project_step(patch_fix_msvc
-  COMMAND ${CMAKE_COMMAND} -E copy_if_different
-          ${SuperBuild_PROJECTS_DIR}/patches/hdf5.config.cmake.ConfigureChecks.cmake
-          <SOURCE_DIR>/config/cmake/ConfigureChecks.cmake
-  DEPENDEES update # do after update
-  DEPENDERS patch  # do before patch
-  )
+  # hdf5 has a bug with MSVC compiler where it doesn't realize its using MSVC
+  # compiler when using nmake or ninja generators. This patch fixes that.
+  add_external_project_step(patch_fix_msvc
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    ${SuperBuild_PROJECTS_DIR}/patches/hdf5.config.cmake.ConfigureChecks.cmake
+    <SOURCE_DIR>/config/cmake/ConfigureChecks.cmake
+    DEPENDEES update # do after update
+    DEPENDERS patch  # do before patch
+    )
 endif()
 
 if (WIN32)
@@ -38,7 +33,7 @@ if (WIN32)
     # This variable is for CGNS, since CGNS doesn't use standard find_package()
     # to find hdf5.
     -DHDF5_LIBRARY:FILEPATH=${install_location}/lib/hdf5dll.lib
-  )
+    )
 
   # On 32-bit Windows, H5public.h ends up redefining ssize_t. This patch ensures
   # that the old definition is undef-ed before redefining it.
