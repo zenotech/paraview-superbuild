cb6ccf7fd25732f0c1ca8ad6dcd196874f492b79
Dan Lipsa <dan.lipsa@kitware.com>
2014-11-21 15:46:18 -0500

    Fix szip install on windows.
    Change-Id: I56e9c3cf112807b6d1d00c65ee24db248a3c93ac

--- Projects/win32/szip.cmake
+++ Projects/win32/szip.cmake
@@ -13,3 +13,13 @@ add_external_project(szip
 add_extra_cmake_args(
     -DSZIP_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/szip.lib
     -DSZIP_INCLUDE_DIR:FILEPATH=<INSTALL_DIR>/include)
+
+# get_filename_component on line 10 is missing the 'component' (path).
+# this patch fixes that.
+add_external_project_step(patch_fix_install
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different
+  ${SuperBuild_PROJECTS_DIR}/patches/szip.config.cmake.SZIP-config.cmake.install.in
+  <SOURCE_DIR>/config/cmake/SZIP-config.cmake.install.in
+  DEPENDEES update # do after update
+  DEPENDERS patch  # do before patch
+  )
