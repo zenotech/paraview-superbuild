dae0bd44ea34460a19f3c437d5894ffa66636c45
Ben Boeckel <ben.boeckel@kitware.com>
2015-01-05 16:34:47 -0500

    boost: force the toolchain on windows
    If multiple Visual Studio installations are available, Boost will pick
    the newest one. Instead, force it to match the version used by the
    superbuild.
    
    Change-Id: Idfee16974a5e88c25777540d820bff39827cc0ea

--- Projects/win32/boost.cmake
+++ Projects/win32/boost.cmake
@@ -1,8 +1,23 @@
+# 8.0 and below unsupported anyways.
+if (MSVC90)
+  set(msvc_ver 9.0)
+elseif (MSVC10)
+  set(msvc_ver 10.0)
+elseif (MSVC11)
+  set(msvc_ver 11.0)
+elseif (MSVC12)
+  set(msvc_ver 12.0)
+elseif (MSVC13)
+  set(msvc_ver 13.0)
+else ()
+  message(FATAL_ERROR "Unrecognized MSVC version")
+endif ()
+
 add_external_project(boost
   DEPENDS zlib
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.bat
-  BUILD_COMMAND <SOURCE_DIR>/b2 --prefix=<INSTALL_DIR> --with-date_time install
+  BUILD_COMMAND <SOURCE_DIR>/b2 --prefix=<INSTALL_DIR> --with-date_time --toolset=msvc-${msvc_ver} install
   INSTALL_COMMAND ""
 )
