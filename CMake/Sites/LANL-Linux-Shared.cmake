28c32bc7d08f5f3ebb649b6b33cbea2603adca0d
Chuck Atkins <chuck.atkins@kitware.com>
2015-01-13 17:29:36 -0500

    Add LANL build scripts
    Change-Id: I73cc18d08fe500bcee102c05c72c5171a7a1bb60

---
+++ CMake/Sites/LANL-Linux-Shared.cmake
@@ -0,0 +1,62 @@
+# Tarballs needing to be downloaded:
+# boost
+# ffmpeg
+# matplotlib
+# numpy
+# osmesa
+# paraview
+# python
+# visitbridge
+
+# General build settings
+set(CMAKE_BUILD_TYPE   Release CACHE STRING "")
+set(BUILD_SHARED_LIBS  ON CACHE BOOL "")
+
+# Enable the appropriate packages
+set(ENABLE_boost       ON CACHE BOOL "")
+set(ENABLE_bzip2       ON CACHE BOOL "")
+set(ENABLE_ffmpeg      ON CACHE BOOL "")
+set(ENABLE_freetype    ON CACHE BOOL "")
+set(ENABLE_lapack      ON CACHE BOOL "")
+set(ENABLE_libxml2     ON CACHE BOOL "")
+set(ENABLE_matplotlib  ON CACHE BOOL "")
+set(ENABLE_mpi         ON CACHE BOOL "")
+set(ENABLE_numpy       ON CACHE BOOL "")
+set(ENABLE_osmesa      ON CACHE BOOL "")
+set(ENABLE_paraview    ON CACHE BOOL "")
+set(ENABLE_paraviewsdk ON CACHE BOOL "")
+set(ENABLE_png         ON CACHE BOOL "")
+set(ENABLE_python      ON CACHE BOOL "")
+set(ENABLE_visitbridge ON CACHE BOOL "")
+set(ENABLE_zlib        ON CACHE BOOL "")
+
+# These will get pulled from the compute node's userland
+set(USE_SYSTEM_freetype ON CACHE BOOL "")
+set(USE_SYSTEM_libxml2  ON CACHE BOOL "")
+set(USE_SYSTEM_png      ON CACHE BOOL "")
+set(USE_SYSTEM_zlib     ON CACHE BOOL "")
+set(USE_SYSTEM_lapack   ON CACHE BOOL "")
+set(USE_SYSTEM_mpi      ON CACHE BOOL "")
+
+# Force the internal python version
+# not sure why we need to do this but otherwise the wrong python version gets
+# found when using older versions of cmake
+set(PV_EXTRA_CMAKE_ARGS
+  "-DPYTHON_EXECUTABLE:FILEPATH=\${install_location}/bin/python\${ep_list_separator}-DPYTHON_LIBRARY:FILEPATH=\${install_location}/lib/libpython2.7.so\${ep_list_separator}-DPYTHON_INCLUDE_DIR:PATH=\${install_location}/include/python2.7"
+  CACHE STRING "")
+
+# Make sure the final ParaView build uses the whole node
+include(ProcessorCount)
+ProcessorCount(N)
+if(NOT N EQUAL 0)
+  set(PV_MAKE_NCPUS ${N} CACHE STRING "")
+else()
+  set(PV_MAKE_NCPUS 5 CACHE STRING "")
+endif()
+
+# Specify where the necessary tarballs have beel downloaded to
+set(download_location /usr/projects/packages/hpc_paraview/superbuild/downloads
+  CACHE PATH "")
+
+# Paraview details
+set(ParaView_FROM_GIT OFF CACHE BOOL "")
