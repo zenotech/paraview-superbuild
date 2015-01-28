791e30e33655a93c71b06b4fe4ef328c500bfe60
Chuck Atkins <chuck.atkins@kitware.com>
2015-01-28 17:19:47 -0500

    Remove unnecessary path to pythonlibs.
    The problem was that if building our own python, the executable
    could get confused and use the system python libraries with our
    own built python executable.  This is no longer an issue since
    we build the superbuild'sm python with rpaths.
    
    Change-Id: Icb12d7d53295cf2b665736d23fedc30e33ee2680

--- Projects/numpy.cmake
+++ Projects/numpy.cmake
@@ -6,21 +6,6 @@ if (WIN32)
 endif()
 
 set(NUMPY_PROCESS_ENVIRONMENT)
-
-# Set up the library path to make sure the right python libs get used
-if(python_ENABLED AND NOT USE_SYSTEM_python)
-  if(UNIX)
-    list(APPEND NUMPY_PROCESS_ENVIRONMENT
-      LD_LIBRARY_PATH "<INSTALL_DIR>/lib:$ENV{LD_LIBRARY_PATH}"
-    )
-  elseif(WIN32)
-    list(APPEND NUMPY_PROCESS_ENVIRONMENT
-      PATH "<INSTALL_DIR>/bin:$ENV{PATH}"
-    )
-  endif()
-endif()
-
-# Make sure that numpy uses the right BLAS and LAPACK
 if(lapack_ENABLED)
   if(NOT LAPACK_FOUND)
     find_package(LAPACK REQUIRED)
@@ -35,9 +20,7 @@ endif()
 
 # If any variables are set, we must have the PROCESS_ENVIRONMENT keyword
 if(NUMPY_PROCESS_ENVIRONMENT)
-  set(NUMPY_PROCESS_ENVIRONMENT
-     PROCESS_ENVIRONMENT ${NUMPY_PROCESS_ENVIRONMENT}
-  )
+  list(INSERT NUMPY_PROCESS_ENVIRONMENT 0 PROCESS_ENVIRONMENT)
 endif()
 
 add_external_project_or_use_system(numpy
