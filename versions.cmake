59b6ad786a9a275158707bdb6d2bb4da3759e55c
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-02-05 09:55:00 -0500

    Update ParaView GIT_TAG to be v4.3.1.
    Change-Id: Ia3d45f03764eaa8cd37001c021de5bcfa80edb16

--- versions.cmake
+++ versions.cmake
@@ -141,7 +141,7 @@ if (ParaView_FROM_GIT)
   # Download PV from GIT
   add_revision(paraview
     GIT_REPOSITORY git://paraview.org/ParaView.git
-    GIT_TAG "master")
+    GIT_TAG "v4.3.1")
 else()
   # Variables to hold the URL and MD5 (optional)
   set (ParaView_URL "http://www.paraview.org/files/v4.3/ParaView-v4.3.1-source.tar.gz" CACHE
