d5d12f7fab9bfd6cd7690c7a83f0eb739f125e28
Ben Boeckel <ben.boeckel@kitware.com>
2015-05-19 10:12:37 -0400

    qt: point to module directories

--- Projects/qt.use.system.cmake
+++ Projects/qt.use.system.cmake
@@ -7,4 +7,9 @@ add_external_dummy_project(qt)
 add_extra_cmake_args(
   -DPARAVIEW_QT_VERSION:STRING=5
   -DQt5_DIR:PATH=${Qt5_DIR}
+  -DQt5Core_DIR:PATH=${Qt5_DIR}/../Qt5Core
+  -DQt5Help_DIR:PATH=${Qt5_DIR}/../Qt5Help
+  -DQt5Network_DIR:PATH=${Qt5_DIR}/../Qt5Network
+  -DQt5Test_DIR:PATH=${Qt5_DIR}/../Qt5Test
+  -DQt5UiTools_DIR:PATH=${Qt5_DIR}/../Qt5UiTools
 )
