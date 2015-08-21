5da5a72d202193529fc6320d57278b5f73bec4fa
Ben Boeckel <ben.boeckel@kitware.com>
2015-08-21 16:52:33 -0400

    diy: factor the MPICC and MPICXX variables out

--- Projects/diy.cmake
+++ Projects/diy.cmake
@@ -1,30 +1,25 @@
 ## Builds DIY library
 ## https://svn.mcs.anl.gov/repos/diy/trunk
+
 if(USE_SYSTEM_mpi)
-  add_external_project(diy
-    DEPENDS mpi hdf5
-    BUILD_IN_SOURCE 1
-    PROCESS_ENVIRONMENT
-      MPICC "${MPI_C_COMPILER}"
-      MPICXX "${MPI_CXX_COMPILER}"
-    CONFIGURE_COMMAND
-        <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-fpic --disable-openmp
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install
-    )
-else()
-  add_external_project(diy
-    DEPENDS mpi hdf5
-    BUILD_IN_SOURCE 1
-    PROCESS_ENVIRONMENT
-      MPICC "<INSTALL_DIR>/bin/mpicc"
-      MPICXX "<INSTALL_DIR>/bin/mpicxx"
-    CONFIGURE_COMMAND
-        <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-fpic --disable-openmp
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install
-    )
+  set(mpicc "${MPI_C_COMPILER}")
+  set(mpicxx "${MPI_CXX_COMPILER}")
+else ()
+  set(mpicc "<INSTALL_DIR>/bin/mpicc")
+  set(mpicxx "<INSTALL_DIR>/bin/mpicxx")
 endif()
+
+add_external_project(diy
+  DEPENDS mpi hdf5
+  BUILD_IN_SOURCE 1
+  PROCESS_ENVIRONMENT
+    MPICC "${mpicc}"
+    MPICXX "${mpicxx}"
+  CONFIGURE_COMMAND
+      <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-fpic --disable-openmp
+  BUILD_COMMAND make
+  INSTALL_COMMAND make install
+  )
 
 # Required because OS X's standard library now has a globally-accessible 'rank'
 # symbol (std::__1::rank) which conflicts with the static-global 'rank' in
