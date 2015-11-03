c234932596497a044353d2232b819e1f37f9fc28
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-11-03 12:45:34 -0500

    Use ospray fork to avoid update issues.
    debian6dash VM is not able to apply patches to git repos properly since
    it has an older git. Hence I'm using a fork for now that has the patchs
    included.

--- Projects/ospray.cmake
+++ Projects/ospray.cmake
@@ -9,16 +9,6 @@ add_external_project(ospray
     -DOSPRAY_BUILD_ISA:STRING=${OSPRAY_BUILD_ISA}
   )
 
-# OSPRay's CMakeLists hard-codes the install path to /tmp/<user>/...
-# This patch fixes that.
-add_external_project_step(patch_fix_install
-  COMMAND ${CMAKE_COMMAND} -E copy_if_different
-  ${SuperBuild_PROJECTS_DIR}/patches/ospray.CMakeLists.txt
-  <SOURCE_DIR>/CMakeLists.txt
-  DEPENDEES update # do after update
-  DEPENDERS patch  # do before patch
-  )
-
 add_extra_cmake_args(
   -DOSPRAY_DIR:PATH=${SuperBuild_BINARY_DIR}/ospray/src/ospray
   -DOSPRAY_CMAKE_DIR:PATH=${SuperBuild_BINARY_DIR}/ospray/src/ospray/cmake/
