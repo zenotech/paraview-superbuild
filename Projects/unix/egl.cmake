1715ec1027b3842d4ccb30fec9451d83c95f319a
Andrew Bauer <andy.bauer@kitware.com>
2016-03-02 13:04:33 -0500

    Improving EGL settings

--- Projects/unix/egl.cmake
+++ Projects/unix/egl.cmake
@@ -1,15 +1,4 @@
-option (ENABLE_egl "Build with EGL offscreen libraries" OFF)
-
-if(ENABLE_egl)
-  find_package(EGL REQUIRED)
-
-  set(egl_ARGS
-    -DEGL_INCLUDE_DIR:PATH=${EGL_INCLUDE_DIR}
-    -DEGL_gldispatch_LIBRARY:FILEPATH=${EGL_gldispatch_LIBRARY}
-    -DEGL_LIBRARY:FILEPATH=${EGL_LIBRARY}
-    -DVTK_USE_OFFSCREEN_EGL:BOOL=ON
-    -DVTK_USE_OFFSCREEN:BOOL=OFF
-    -DVTK_USE_X:BOOL=OFF
-    )
-
-endif(ENABLE_egl)
+message(STATUS "Using system EGL. Pick correct python based on your deployment target")
+add_external_project_or_use_system(egl)
+set(USE_SYSTEM_egl TRUE CACHE BOOL "" FORCE)
+mark_as_advanced(USE_SYSTEM_egl)
