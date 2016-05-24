d0ecd6db7957088c90a1beed3c74fd4b93db5d97
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2016-05-24 12:03:08 -0400

    Add rules to package getting started guide and tutorial.
    Also refactored logic to download/package all PDFs.

--- Projects/apple/paraview.bundle.cmake
+++ Projects/apple/paraview.bundle.cmake
@@ -149,6 +149,18 @@ if (CUSTOMIZE_DMG)
           COMPONENT superbuild)
 endif()
 
+if (paraviewgettingstartedguide_ENABLED)
+  install(FILES ${paraviewgettingstartedguide_pdf}
+    DESTINATION "paraview.app/Contents/doc"
+    COMPONENT superbuild)
+endif()
+if (paraviewusersguide_ENABLED)
+  install(FILES ${paraviewusersguide_pdf}
+          DESTINATION "paraview.app/Contents/doc"
+          COMPONENT superbuild)
+endif()
+
+
 add_test(NAME GenerateParaViewPackage
          COMMAND ${CMAKE_CPACK_COMMAND} -G DragNDrop -V
          WORKING_DIRECTORY ${SuperBuild_BINARY_DIR})
