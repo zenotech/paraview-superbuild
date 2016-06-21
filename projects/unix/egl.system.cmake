80f861f0c0e8d3b5a7572b8a4005977e5d70a910
Ben Boeckel <ben.boeckel@kitware.com>
2016-06-21 17:34:45 -0400

    egl: port

--- projects/unix/egl.system.cmake
+++ projects/unix/egl.system.cmake
@@ -2,11 +2,10 @@ find_package(EGL REQUIRED)
 
 # This will add PYTHON_LIBRARY, PYTHON_EXECUTABLE, PYTHON_INCLUDE_DIR
 # variables. User can set/override these to change the Python being used.
-add_extra_cmake_args(
+superbuild_add_extra_cmake_args(
   -DEGL_INCLUDE_DIR:PATH=${EGL_INCLUDE_DIR}
   -DEGL_gldispatch_LIBRARY:FILEPATH=${EGL_gldispatch_LIBRARY}
   -DEGL_LIBRARY:FILEPATH=${EGL_LIBRARY}
   -DVTK_USE_OFFSCREEN_EGL:BOOL=ON
   -DVTK_USE_OFFSCREEN:BOOL=OFF
-  -DVTK_USE_X:BOOL=OFF
-)
+  -DVTK_USE_X:BOOL=OFF)
