8d4bee403632160447492bd739a2c329e05ce858
Ben Boeckel <ben.boeckel@kitware.com>
2016-07-08 15:50:46 -0400

    paraviewweb: require python

--- projects/paraviewweb.cmake
+++ projects/paraviewweb.cmake
@@ -5,4 +5,4 @@ if (WIN32)
 endif ()
 
 superbuild_add_dummy_project(paraviewweb
-  DEPENDS paraviewwebvisualizer ${paraviewweb_depends})
+  DEPENDS python paraviewwebvisualizer ${paraviewweb_depends})
