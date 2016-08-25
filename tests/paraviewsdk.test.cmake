237b83eb0735c0e2bb7705b5c89322b7cf5827f9
Ben Boeckel <ben.boeckel@kitware.com>
2016-08-25 14:08:00 -0400

    paraviewsdk: add tests

--- tests/paraviewsdk.test.cmake
+++ tests/paraviewsdk.test.cmake
@@ -1,21 +1,109 @@
-#------------------------------------------------------------------------------
-# Test to check install tree for paraviewsdk
-#------------------------------------------------------------------------------
-set(paraview_source_dir "<SOURCE_DIR>")
-set(paraview_binary_dir "<BINARY_DIR>")
-_ep_replace_location_tags(paraview paraview_source_dir)
-_ep_replace_location_tags(paraview paraview_binary_dir)
+set(paraviewsdk_extract_dir "${CMAKE_CURRENT_BINARY_DIR}/paraviewsdk/test-extraction")
+if (WIN32)
+  set(generator "ZIP")
+elseif (APPLE)
+  set(generator "DragNDrop")
+else ()
+  set(generator "TGZ")
+endif ()
 
-add_test(
-  NAME    paraviewsdk-install
-  COMMAND "${CMAKE_COMMAND}"
-          -DPARAVIEW_BINARY_DIR:PATH=${paraview_binary_dir}
-          -DPARAVIEW_INSTALL_DIR:PATH=${superbuild_install_directory}
-          -DPARAVIEW_SOURCE_DIR:PATH=${paraview_source_dir}
-          -DPARAVIEW_TEST_DIR:PATH=${CMAKE_BINARY_DIR}/Testing/Temporary
-          -DPARAVIEW_VERSION:STRING=${paraview_version}
-          -P ${CMAKE_CURRENT_LIST_DIR}/scripts/paraviewsdk.test.install.cmake)
-set_tests_properties(paraviewsdk-install
-  PROPERTIES
-    LABELS  "PARAVIEW"
-    TIMEOUT 1500)
+if (PARAVIEW_PACKAGE_FILE_NAME)
+  set(glob_prefix "${PARAVIEW_PACKAGE_FILE_NAME}")
+else ()
+  include(paraview.suffix)
+  set(glob_prefix "ParaViewSDK-${paraview_version_major}.${paraview_version_minor}.${paraview_version_patch}")
+  if (PARAVIEW_PACKAGE_SUFFIX)
+    set(glob_prefix "${glob_prefix}-${PARAVIEW_PACKAGE_SUFFIX}")
+  endif ()
+endif ()
+superbuild_add_extract_test("paraviewsdk" "${glob_prefix}" "TGZ" "${paraviewsdk_extract_dir}"
+  LABELS "ParaView\;ParaViewSDK")
+
+set(paraview_source_dir "<SOURCE_DIR>")
+_ep_replace_location_tags(paraview paraview_source_dir)
+
+set(paraviewsdk_test_dir "${CMAKE_CURRENT_BINARY_DIR}/paraviewsdk/tests")
+function (add_paraviewsdk_example_test name dir)
+  add_test(
+    NAME    "paraviewsdk-example-${name}"
+    COMMAND "${CMAKE_CTEST_COMMAND}"
+            --build-and-test
+            "${paraview_source_dir}/Examples/${dir}"
+            "${paraviewsdk_test_dir}/examples/${name}"
+            --build-generator "${CMAKE_GENERATOR}"
+            --build-options
+            "-DParaView_DIR:PATH=${paraviewsdk_extract_dir}/lib/cmake/paraview-${paraview_version}")
+  set_tests_properties("paraviewsdk-example-${name}"
+    PROPERTIES
+      DEPENDS "extract-paraviewsdk-TGZ"
+      LABELS  "ParaView;ParaViewSDK"
+      TIMEOUT 1200)
+endfunction ()
+
+function (add_paraviewsdk_example_app_test name)
+  string(TOLOWER "${name}" lower_name)
+  add_paraviewsdk_example_test("app-${lower_name}" "CustomApplications/${name}")
+endfunction ()
+
+function (add_paraviewsdk_example_catalyst_test name)
+  string(TOLOWER "${name}" lower_name)
+  add_paraviewsdk_example_test("catalyst-${lower_name}" "Catalyst/${name}")
+endfunction ()
+
+function (add_paraviewsdk_example_plugin_test name)
+  string(TOLOWER "${name}" lower_name)
+  add_paraviewsdk_example_test("plugin-${lower_name}" "Plugins/${name}")
+endfunction ()
+
+set(gui_enabled FALSE)
+if (qt4_enabled OR qt5_enabled)
+  set(gui_enabled TRUE)
+endif ()
+
+if (gui_enabled)
+  add_paraviewsdk_example_app_test(Spreadsheet)
+  add_paraviewsdk_example_app_test(Clone1)
+  add_paraviewsdk_example_app_test(Clone2)
+  add_paraviewsdk_example_app_test(Demo0)
+  add_paraviewsdk_example_app_test(Demo1)
+  add_paraviewsdk_example_app_test(MultiServerClient)
+  add_paraviewsdk_example_app_test(ParticlesViewer)
+endif ()
+
+add_paraviewsdk_example_catalyst_test(CFullExample)
+add_paraviewsdk_example_catalyst_test(CFullExample2)
+add_paraviewsdk_example_catalyst_test(CxxFullExample)
+add_paraviewsdk_example_catalyst_test(CxxImageDataExample)
+add_paraviewsdk_example_catalyst_test(CxxMappedDataArrayExample)
+add_paraviewsdk_example_catalyst_test(CxxMultiPieceExample)
+add_paraviewsdk_example_catalyst_test(CxxNonOverlappingAMRExample)
+add_paraviewsdk_example_catalyst_test(CxxOverlappingAMRExample)
+add_paraviewsdk_example_catalyst_test(CxxPVSMPipelineExample)
+add_paraviewsdk_example_catalyst_test(CxxParticlePathExample)
+add_paraviewsdk_example_catalyst_test(CxxSOADataArrayExample)
+add_paraviewsdk_example_catalyst_test(CxxVTKPipelineExample)
+if (NOT CMAKE_GENERATOR MATCHES "Ninja")
+  add_paraviewsdk_example_catalyst_test(Fortran90FullExample)
+  add_paraviewsdk_example_catalyst_test(FortranPoissonSolver)
+endif ()
+add_paraviewsdk_example_catalyst_test(MPISubCommunicatorExample)
+add_paraviewsdk_example_catalyst_test(PythonDolfinExample)
+
+if (gui_enabled)
+  add_paraviewsdk_example_plugin_test(Autostart)
+  add_paraviewsdk_example_plugin_test(RepresentationBehavior)
+endif ()
+add_paraviewsdk_example_plugin_test(DockWidget)
+add_paraviewsdk_example_plugin_test(Filter)
+add_paraviewsdk_example_plugin_test(GUIToolBar)
+add_paraviewsdk_example_plugin_test(HiddenLinesRemoval)
+add_paraviewsdk_example_plugin_test(ParametricSource)
+add_paraviewsdk_example_plugin_test(PropertyWidgets)
+add_paraviewsdk_example_plugin_test(Reader)
+add_paraviewsdk_example_plugin_test(ReaderXMLOnly)
+add_paraviewsdk_example_plugin_test(SMProxy)
+add_paraviewsdk_example_plugin_test(SourceToolbar)
+if (visitbridge_enabled)
+  add_paraviewsdk_example_plugin_test(VisItReader)
+endif ()
+add_paraviewsdk_example_plugin_test(Writer)
