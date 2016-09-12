3c4df02b6e351f968a0280062302f6d4f17d15c8
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-12 09:42:26 -0400

    paraview.plugin: write artifacts to the top-level dir

--- projects/paraview.plugin.cmake
+++ projects/paraview.plugin.cmake
@@ -39,7 +39,7 @@ superbuild_add_project("${plugin_project}"
     "${CMAKE_COMMAND}"
       -Dparaview_binary_location:PATH=${paraview_binary_dir}
       -Dplugin_name:STRING=${${plugin_project}_name}
-      -Dbundle_name:STRING=${CMAKE_CURRENT_BINARY_DIR}/${${plugin_project}_name}
+      -Dbundle_name:STRING=${CMAKE_BINARY_DIR}/${${plugin_project}_name}
       -Dbundle_suffix_file:STRING=${_superbuild_module_gen_dir}/paraview-version.cmake
       -Dtmp_location:PATH=<TMP_DIR>
       "-Dinstall_files:STRING=${${plugin_project}_install_files}"
