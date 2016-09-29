786294cf8db3ba33674cf19baee8b9079ab4fa42
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-29 11:11:03 -0400

    cgns: clean up the logic a bit
    The values should always be set so that the cache is never out of date.

--- projects/cgns.cmake
+++ projects/cgns.cmake
@@ -1,20 +1,22 @@
-
+set(cgns_need_szip OFF)
+set(cgns_need_zlib OFF)
 if (hdf5_built_by_superbuild)
   if (szip_enabled)
-    set(cgns_hdf5_szip -DHDF5_NEED_SZIP=ON)
+    set(cgns_need_szip ON)
   endif ()
   if (zlib_enabled)
-    set(cgns_hdf5_zlib -DHDF5_NEED_ZLIB=ON)
+    set(cgns_need_zlib ON)
   endif ()
 endif ()
+
 superbuild_add_project(cgns
   DEPENDS_OPTIONAL zlib szip hdf5
   CMAKE_ARGS
     -DCGNS_BUILD_SHARED:BOOL=${BUILD_SHARED_LIBS}
     -DCGNS_ENABLE_64BIT:BOOL=ON
     -DCGNS_ENABLE_HDF5:BOOL=${hdf5_enabled}
-    ${cgns_hdf5_szip}
-    ${cgns_hdf5_zlib})
+    -DHDF5_NEED_SZIP:BOOL=${cgns_need_szip}
+    -DHDF5_NEED_ZLIB:BOOL=${cgns_need_zlib})
 
 superbuild_apply_patch(cgns windows-install
   "Fix installation of libraries on Windows")
