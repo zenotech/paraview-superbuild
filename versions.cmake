5414c18232a091483e91db1fe4a8c236841cfb54
Cory Quammen <cory.quammen@kitware.com>
2016-10-07 16:01:28 -0400

    Update to v5.2.0-RC1

--- versions.cmake
+++ versions.cmake
@@ -52,9 +52,9 @@ superbuild_set_revision(paraviewtutorialdata
   URL_MD5 ff7ceab8cfc674b227c0bba392d1ed3c)
 
 superbuild_set_external_source(paraview
-  "https://gitlab.kitware.com/paraview/paraview.git" "origin/master"
-  "http://www.paraview.org/files/v5.1/ParaView-v5.1.2.tar.gz"
-  "44fb32fc8988fcdfbc216c9e40c3e925")
+  "https://gitlab.kitware.com/paraview/paraview.git" "v5.2.0-RC1"
+  "http://www.paraview.org/files/v5.2/ParaView-v5.2.0-RC1.tar.gz"
+  "5a51e5f70b95fd7643f5f83dd5c37094")
 
 get_property(paraview_revision GLOBAL
   PROPERTY paraview_revision)
