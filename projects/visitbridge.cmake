284d06e278eb36c38478c7c2d839be51c57cedc8
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2016-10-12 08:12:24 -0400

    `glu` only available on linux

--- projects/visitbridge.cmake
+++ projects/visitbridge.cmake
@@ -1,2 +1,7 @@
-superbuild_add_dummy_project(visitbridge
-  DEPENDS boost glu)
+if (UNIX AND NOT APPLE)
+  superbuild_add_dummy_project(visitbridge
+    DEPENDS boost glu)
+else ()
+  superbuild_add_dummy_project(visitbridge
+    DEPENDS boost)
+endif ()
