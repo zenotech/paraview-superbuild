54a568c39e1cff619af95f24f0afe8e81b0cd1dc
Ben Boeckel <ben.boeckel@kitware.com>
2017-02-01 10:19:41 -0500

    vortexfinder2: use the qt project

--- projects/vortexfinder2.cmake
+++ projects/vortexfinder2.cmake
@@ -1,6 +1,7 @@
 paraview_add_plugin(vortexfinder2
   PLUGIN_NAME VortexFinder2
-  DEPENDS boost qt4)
+  DEPENDS boost qt
+  DEPENDS_OPTIONAL qt4 qt5)
 
 # XXX: Remove when https://github.com/tjcorona/vortexfinder2/pull/2 is merged.
 superbuild_add_extra_cmake_args(
