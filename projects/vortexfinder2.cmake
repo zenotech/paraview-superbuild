9631055106ff6dd7f9a5ce4f99a781742a1c2005
Ben Boeckel <ben.boeckel@kitware.com>
2017-02-13 13:23:54 -0500

    vortexfinder2: update to upstream master
    All of our patches have been merged, so use a new tarball.

--- projects/vortexfinder2.cmake
+++ projects/vortexfinder2.cmake
@@ -1,14 +1,7 @@
 paraview_add_plugin(vortexfinder2
   PLUGIN_NAME VortexFinder2
-  DEPENDS boost qt
+  DEPENDS boost qt cxx11
   DEPENDS_OPTIONAL qt4 qt5)
 
-# XXX: Remove when https://github.com/tjcorona/vortexfinder2/pull/2 is merged.
 superbuild_add_extra_cmake_args(
-  -DWITH_CXX11:BOOL=${cxx11_enabled}
   -DWITH_MACOS_RPATH:BOOL=FALSE)
-
-superbuild_apply_patch(vortexfinder2 library-install
-  "Use VTK's install library directory")
-superbuild_apply_patch(vortexfinder2 rpath-option
-  "Make @rpath/ usage optional")
