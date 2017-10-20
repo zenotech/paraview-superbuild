baa26d83f13bdabefa4b2e31258eb51e5ce2f47b
Dan Lipsa <dan.lipsa@kitware.com>
2017-10-20 16:53:35 -0400

    Build only what is needed.

--- projects/las.cmake
+++ projects/las.cmake
@@ -3,7 +3,9 @@ superbuild_add_project(las
   CMAKE_ARGS
     -DWITH_GDAL:BOOL=FALSE
     -DWITH_GEOTIFF:BOOL=FALSE
-    -DWITH_LASZIP:BOOL=FALSE)
+    -DWITH_LASZIP:BOOL=FALSE
+    -DWITH_TESTS:BOOL=FALSE
+    -DWITH_UTILITIES:BOOL=FALSE)
 
 superbuild_apply_patch(las respect-with-geotiff
   "find_package GeoTIFF only if WITH_GEOTIFF")
