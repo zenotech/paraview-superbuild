beeb5f6fe49e32a3a377522a4bddd147b80e6a6e
Ben Boeckel <ben.boeckel@kitware.com>
2017-10-27 11:18:01 -0400

    las: avoid an unnecessary variable abstraction

--- projects/apple-unix/las.cmake
+++ projects/apple-unix/las.cmake
@@ -1,21 +1,13 @@
-set(las_cmake_args
-  -DWITH_GDAL:BOOL=FALSE
-  -DBUILD_OSGEO4W:BOOL=OFF
-  -DWITH_GEOTIFF:BOOL=FALSE
-  -DWITH_LASZIP:BOOL=FALSE
-  -DWITH_TESTS:BOOL=FALSE
-  -DWITH_UTILITIES:BOOL=FALSE)
-
-# las sets, on windows, different boost options than the default
-if(WIN32 AND MSVC)
-  list(APPEND las_cmake_args -DBoost_USE_STATIC_LIBS:BOOL=FALSE)
-endif()
-
 superbuild_add_project(las
   DEPENDS boost
   CMAKE_ARGS
-  ${las_cmake_args}
-  )
+    -DWITH_GDAL:BOOL=FALSE
+    -DBUILD_OSGEO4W:BOOL=OFF
+    -DWITH_GEOTIFF:BOOL=FALSE
+    -DWITH_LASZIP:BOOL=FALSE
+    -DWITH_TESTS:BOOL=FALSE
+    -DWITH_UTILITIES:BOOL=FALSE
+    -DBoost_USE_STATIC_LIBS:BOOL=FALSE)
 
 # this patch is commited upstream at 4dbc30a7e7e099cbe01a7c192ec19d231cc26894
 superbuild_apply_patch(las respect-with-geotiff
