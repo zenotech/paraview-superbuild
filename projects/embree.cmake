139bc9302da2793ba8c836acfefb6354eba5fa3a
Chuck Atkins <chuck.atkins@kitware.com>
2018-02-28 11:42:12 -0500

    Update embree's usage of ISA and TBB CMake variables
    (cherry picked from commit c4477a4072039a14473ae3b17499fedad589adb3)

--- projects/embree.cmake
+++ projects/embree.cmake
@@ -5,6 +5,7 @@ if(NOT (embree_BUILD_ISA STREQUAL "DEFAULT"))
   if(embree_BUILD_ISA STREQUAL "ALL")
     set(embree_BUILD_ISA SSE2 SSE42 AVX AVX2 AVX512KNL AVX512SKX)
   endif()
+  list(APPEND embree_isa_args -DEMBREE_MAX_ISA:BOOL=NONE)
   foreach(isa IN LISTS embree_BUILD_ISA)
     list(APPEND embree_isa_args -DEMBREE_ISA_${isa}:BOOL=ON)
   endforeach()
@@ -14,7 +15,6 @@ superbuild_add_project(embree
   DEPENDS ispc tbb cxx11
   CMAKE_ARGS
     ${embree_isa_args}
-    -DEMBREE_TBB_ROOT:PATH=<INSTALL_DIR>
     -DEMBREE_ISPC_EXECUTABLE:PATH=<INSTALL_DIR>/bin/ispc
     -DEMBREE_GEOMETRY_HAIR:BOOL=OFF
     -DEMBREE_GEOMETRY_LINES:BOOL=OFF
