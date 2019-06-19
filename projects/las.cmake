dddc72a10f649a1f15b40faa671a7f8ff66fb3ab
Ben Boeckel <ben.boeckel@kitware.com>
2019-06-19 16:33:16 -0400

    las: use rpaths on Linux

--- projects/las.cmake
+++ projects/las.cmake
@@ -1,3 +1,9 @@
+set(las_configure_flags)
+if (UNIX AND NOT APPLE)
+  list(APPEND las_configure_flags
+    -DCMAKE_INSTALL_RPATH:STRING=<INSTALL_DIR>/lib)
+endif ()
+
 superbuild_add_project(las
   DEPENDS boost
   CMAKE_ARGS
@@ -7,7 +13,8 @@ superbuild_add_project(las
     -DWITH_LASZIP:BOOL=FALSE
     -DWITH_TESTS:BOOL=FALSE
     -DWITH_UTILITIES:BOOL=FALSE
-    -DBoost_USE_STATIC_LIBS:BOOL=FALSE)
+    -DBoost_USE_STATIC_LIBS:BOOL=FALSE
+    ${las_configure_flags})
 
 # this patch is commited upstream at 4dbc30a7e7e099cbe01a7c192ec19d231cc26894
 superbuild_apply_patch(las respect-with-geotiff
@@ -18,6 +25,9 @@ superbuild_apply_patch(las enable-outside-boost-options
 
 superbuild_apply_patch(las add-boost-include-dirs
   "Boost include dirs are needed on Windows")
+
+superbuild_apply_patch(las allow-rpath
+  "Don't reject RPATH settings on Linux")
 
 if (WIN32)
   superbuild_append_flags(cxx_flags "-DBOOST_ALL_NO_LIB" PROJECT_ONLY)
