568ceb78c67d117acabf1b81c72653c161b4ba3c
Scott Wittenburg <scott.wittenburg@kitware.com>
2019-06-19 13:57:00 -0600

    Docker: Replace example shell scripts with actual documentation

--- Scripts/docker/ubuntu/deployme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# docker login (+ enter username and password)
-docker tag pv-v5.6.0-egl kitware/paraviewweb:pv-v5.6.0-egl
-docker push kitware/paraviewweb:pv-v5.6.0-egl
--- Scripts/docker/ubuntu/runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# If you want to run the OSMesa version, no runtime arg is needed:
-docker run --rm -ti pv-v5.6.0-osmesa
-
-# Or if you have nvidia-docker2 installed and want to run the egl version:
-# docker run --rm --runtime=nvidia -ti pv-v5.6.0-egl
