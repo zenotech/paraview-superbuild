059e2fa082b1234aea6d6be293b905f97599b736
Ben Boeckel <ben.boeckel@kitware.com>
2019-10-03 10:40:06 -0400

    vrpn: force GPM support to be off
    Fixes: #129

--- projects/vrpn.cmake
+++ projects/vrpn.cmake
@@ -1,3 +1,7 @@
 superbuild_add_project(vrpn
   CMAKE_ARGS
+    # GPM support is enabled if GPM is found on the machine. This later ends up
+    # complaining that it is GPL stuff and needs another flag to work. We never
+    # want this, so just always disable it.
+    -DVRPN_USE_GPM_MOUSE:BOOL=OFF
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS})
