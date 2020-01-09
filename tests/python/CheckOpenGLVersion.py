f55e39258401a7b1265e9b8cad600f6229c58222
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-01-09 12:39:46 -0500

    fix imports in CheckOpenGLVersion test.

--- tests/python/CheckOpenGLVersion.py
+++ tests/python/CheckOpenGLVersion.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import sys
-from paraview.modules.vtkPVClientServerCoreRendering import vtkPVOpenGLInformation
+from paraview.modules.vtkRemotingViews import vtkPVOpenGLInformation
 
 if len(sys.argv) == 1:
     print("Usage: %s [strings to match]" % sys.argv[0])
