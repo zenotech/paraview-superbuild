c38977ddc6856b67564504e8b29391ae948175e4
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-01-30 11:39:36 -0500

    centos7: add base docker image.

---
+++ .gitlab/ci/docker/centos7/install_deps.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Install build requirements.
+yum install -y \
+    zlib-devel libcurl-devel python-devel \
+    freeglut-devel glew-devel graphviz-devel libpng-devel \
+    libxcb libxcb-devel libXt-devel xcb-util xcb-util-devel mesa-libGL-devel mesa-libEGL-devel \
+    libxkbcommon-devel file mesa-dri-drivers autoconf automake libtool chrpath
+
+# Install EPEL
+yum install -y \
+    epel-release
+
+# Install development tools
+yum install -y \
+    git-core \
+    git-lfs
+
+# Install toolchains.
+yum install -y \
+    centos-release-scl
+yum install -y \
+    devtoolset-7-gcc-c++ \
+    devtoolset-7 \
+    devtoolset-7-gcc \
+    devtoolset-7-gfortran
+
+yum clean all
