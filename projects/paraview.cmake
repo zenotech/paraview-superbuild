a73f587bf373180cae099c8995232449e3ae185a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-02-01 12:15:32 -0500

    paraview: correct enable Fortran support.

--- projects/paraview.cmake
+++ projects/paraview.cmake
@@ -139,7 +139,7 @@ superbuild_add_project(paraview
   DEBUGGABLE
   DEFAULT_ON
   DEPENDS_OPTIONAL
-    adios2 cuda boost gdal hdf5 matplotlib mpi numpy png protobuf
+    adios2 cuda boost fortran gdal hdf5 matplotlib mpi numpy png protobuf
     python python2 python3 qt5 visitbridge zlib silo las
     xdmf3 ospray vrpn vtkm tbb netcdf
     nlohmannjson
@@ -171,6 +171,7 @@ superbuild_add_project(paraview
     -DPARAVIEW_PLUGIN_ENABLE_OpenVR:BOOL=${openvr_enabled}
     -DPARAVIEW_PYTHON_VERSION:STRING=${python_version}
     -DPARAVIEW_USE_MPI:BOOL=${mpi_enabled}
+    -DPARAVIEW_USE_FORTRAN:BOOL=${fortran_enabled}
     -DPARAVIEW_USE_PYTHON:BOOL=${PARAVIEW_USE_PYTHON}
     -DPARAVIEW_USE_QT:BOOL=${qt5_enabled}
     -DVISIT_BUILD_READER_Mili:BOOL=${mili_enabled}
