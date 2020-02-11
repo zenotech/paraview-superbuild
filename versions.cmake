9bb8e2e213b7e4d839c96dc3845bb05b6a7a5ab6
Cory Quammen <cory.quammen@kitware.com>
2020-02-11 07:59:35 -0500

    Update the default version to 5.8.0-RC3

--- versions.cmake
+++ versions.cmake
@@ -39,9 +39,9 @@ superbuild_set_revision(paraviewtutorialdata
 # on this assumption.
 superbuild_set_selectable_source(paraview
   # NOTE: When updating this selection, also update `README.md` and CMakeLists.txt.
-  SELECT 5.8.0-RC2 DEFAULT
-    URL     "http://www.paraview.org/files/v5.8/ParaView-v5.8.0-RC2.tar.xz"
-    URL_MD5 c4b4b94ac9376c488043e2d96fd7e868
+  SELECT 5.8.0-RC3 DEFAULT
+    URL     "http://www.paraview.org/files/v5.8/ParaView-v5.8.0-RC3.tar.xz"
+    URL_MD5 355428c31455db996b780a1a44ebc5f9
   SELECT git CUSTOMIZABLE
     GIT_REPOSITORY "https://gitlab.kitware.com/paraview/paraview.git"
     GIT_TAG        "origin/master"
