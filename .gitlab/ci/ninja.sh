2610c06e4e598733b6e12f74d4c790be031b3df8
Ben Boeckel <ben.boeckel@kitware.com>
2020-09-08 15:27:49 -0400

    ci: add support for macOS

--- .gitlab/ci/ninja.sh
+++ .gitlab/ci/ninja.sh
@@ -2,15 +2,34 @@
 
 set -e
 
-readonly version="1.9.0.g99df1.kitware.dyndep-1.jobserver"
-readonly sha256sum="15d1315616b97142217bd18e88ddd5f60a1ff5c14a19e22a4bf50681898cf53c"
-readonly filename="ninja-$version-1_x86_64-linux-gnu"
-readonly tarball="$filename.tar.gz"
+readonly version="1.10.0"
+
+case "$( uname -s )" in
+    Linux)
+        shatool="sha256sum"
+        sha256sum="6566836ddf3d72ca06685b34814e0c6fa0f0943542d651d0dab3150f10307c82"
+        platform="linux"
+        ;;
+    Darwin)
+        shatool="shasum -a 256"
+        sha256sum="2ee405c0e205d55666c60cc9c0d8d04c8ede06d3ef2e2c2aabe08fd81c17d22e"
+        platform="mac"
+        ;;
+    *)
+        echo "Unrecognized platform $( uname -s )"
+        exit 1
+        ;;
+esac
+readonly shatool
+readonly sha256sum
+readonly platform
+
+readonly filename="ninja-$platform"
+readonly tarball="$filename.zip"
 
 cd .gitlab
 
 echo "$sha256sum  $tarball" > ninja.sha256sum
-curl -OL "https://github.com/Kitware/ninja/releases/download/v$version-1/$tarball"
-sha256sum --check ninja.sha256sum
-tar xf "$tarball"
-mv "$filename/ninja" .
+curl -OL "https://github.com/ninja-build/ninja/releases/download/v$version/$tarball"
+$shatool --check ninja.sha256sum
+./cmake/bin/cmake -E tar xf "$tarball"
