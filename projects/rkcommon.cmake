fa5b9de92bde3308a99a0817239a1d3fee45ba26
David E. DeMarle <david.demarle@intel.com>
2020-09-10 09:22:40 -0400

    backport a feature that allows ospray to search in user paths
    the patch is in rkcommon devel and will be uneccessary when
    PVSB's rkcommon version is bumped to the to be released 1.5.0

--- projects/rkcommon.cmake
+++ projects/rkcommon.cmake
@@ -6,3 +6,5 @@ superbuild_add_project(rkcommon
      -DCMAKE_INSTALL_NAME_DIR:PATH=<INSTALL_DIR>/lib
      -DINSTALL_DEPS:BOOL=OFF
 )
+
+superbuild_apply_patch(rkcommon allow-library-paths "assist with dynamic library resolution")
