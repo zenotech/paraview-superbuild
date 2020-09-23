1626382818fa90eccfb1cf23704dc866e9131248
Ben Boeckel <ben.boeckel@kitware.com>
2020-09-23 10:20:01 -0400

    ci: add an rpath entry for our Fortran toolchain

--- .gitlab/ci/configure_macos.cmake
+++ .gitlab/ci/configure_macos.cmake
@@ -1,1 +1,4 @@
+# Add the rpath for fortran libraries.
+set(_superbuild_fortran_ld_flags "-Wl,-rpath,$ENV{CI_PROJECT_DIR}/.gitlab/gfortran/lib" CACHE STRING "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
