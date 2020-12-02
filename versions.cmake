5528808c02d6a8d3df0f0c198d0bce3b4d24b700
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-12-02 11:38:20 -0500

    add openpmd

--- versions.cmake
+++ versions.cmake
@@ -247,3 +247,7 @@ superbuild_set_revision(gdal
 
 superbuild_set_revision(launchers
   SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/launchers")
+
+superbuild_set_revision(openpmd
+  URL     "https://www.paraview.org/files/dependencies/openPMD-api-0.12.0-alpha.tar.gz"
+  URL_MD5 "ada986539b4d2cc004888f161f41de85")
