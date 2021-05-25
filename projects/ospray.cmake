ed35ec09441785de5d0803a2d1552c5ddc207dbe
Ben Boeckel <ben.boeckel@kitware.com>
2021-05-25 15:42:14 -0400

    ospray: avoid unnecessary variable indirection

--- projects/ospray.cmake
+++ projects/ospray.cmake
@@ -9,11 +9,9 @@ set(ospray_BUILD_ISA "${ospray_isa_default}"
 mark_as_advanced(ospray_BUILD_ISA)
 set_property(CACHE ospray_BUILD_ISA PROPERTY STRINGS SSE AVX AVX2 AVX512KNL AVX512SKX ALL)
 
-set (ospray_depends ispc tbb cxx11 embree ospraymaterials openimagedenoise
-  rkcommon openvkl)
-
 superbuild_add_project(ospray
-  DEPENDS ${ospray_depends}
+  DEPENDS
+    ispc tbb cxx11 embree ospraymaterials openimagedenoise rkcommon openvkl
   CMAKE_ARGS
     -DOSPRAY_ISPC_DIRECTORY:PATH=<INSTALL_DIR>/bin
     -DCMAKE_INSTALL_NAME_DIR:PATH=<INSTALL_DIR>/lib
