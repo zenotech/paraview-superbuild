e6844d8ab3cea10bea3c601e17e2132df1aae943
Ben Boeckel <ben.boeckel@kitware.com>
2021-08-03 18:36:39 -0400

    ttk: simplify module selection logic
    Instead of spelling out `VTK_MODULE_ENABLE_` for each module, just keep
    a list of modules to enable and create the flag in the same loop as the
    user-provided variables.

--- projects/ttk.cmake
+++ projects/ttk.cmake
@@ -4,113 +4,97 @@
 # Modules
 # -------
 
-# core modules
-set(TTK_CORE_FILTER_LIST)
+set(ttk_enabled_modules
+  # core modules
+  ttkAlgorithm
+  ttkTriangulationAlgorithm
+  ttkProgramBase
 
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkAlgorithm=YES
-  -DVTK_MODULE_ENABLE_ttkTriangulationAlgorithm=YES
-  -DVTK_MODULE_ENABLE_ttkProgramBase=YES
+  # tests
+  ttkBlank
+  ttkHelloWorld
+
+  # utility
+  ttkArrayEditor
+  ttkArrayPreconditioning
+  ttkBlockAggregator
+  ttkDataSetToTable
+  ttkExtract
+  ttkForEach
+  ttkEndFor
+  ttkGridLayout
+  ttkIcosphere
+  ttkIcosphereFromObject
+  ttkIcospheresFromPoints
+  ttkIdentifierRandomizer
+  ttkPointDataSelector
+  ttkScalarFieldNormalizer
+  ttkScalarFieldSmoother
+  ttkTextureMapFromField
+
+  # topology
+  ttkContinuousScatterPlot
+  ttkContourAroundPoint
+  ttkContourTreeAlignment
+  ttkFTMTree
+  ttkFTRGraph
+  ttkFiber
+  ttkFiberSurface
+  ttkPersistenceCurve
+  ttkPersistenceDiagram
+  ttkIntegralLines
+  ttkJacobiSet
+  ttkMeshGraph
+  ttkMorseSmaleComplex
+  ttkPeriodicGrid
+  ttkPlanarGraphLayout
+  ttkReebSpace
+  ttkScalarFieldCriticalPoints
+  ttkTopologicalSimplification
+  ttkTrackingFromFields
+  ttkTrackingFromOverlap
+  ttkTrackingFromPersistenceDiagrams
+
+  # geometry processing
+  ttkDepthImageBasedApproximation
+  ttkDistanceField
+  ttkGeometrySmoother
+  ttkManifoldCheck
+  ttkMorseSmaleQuadrangulation
+  ttkProjectionFromField
+  ttkQuadrangulationSubdivision
+
+  # clustering
+  ttkPersistenceDiagramClustering
+
+  # ensemble
+  ttkMandatoryCriticalPoints
+  ttkDimensionReduction
+
+  # cinema
+  ttkCinemaReader
+  ttkCinemaWriter
+  ttkCinemaQuery
+  ttkCinemaImaging
+  ttkCinemaProductReader
+  ttkCinemaDarkroom
+
+  # compression
+  ttkTopologicalCompressionReader
+  ttkTopologicalCompressionWriter
 )
 
-# tests
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkBlank=YES
-  -DVTK_MODULE_ENABLE_ttkHelloWorld=YES
-)
-
-# utility
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkArrayEditor=YES
-  -DVTK_MODULE_ENABLE_ttkArrayPreconditioning=YES
-  -DVTK_MODULE_ENABLE_ttkBlockAggregator=YES
-  -DVTK_MODULE_ENABLE_ttkDataSetToTable=YES
-  -DVTK_MODULE_ENABLE_ttkExtract=YES
-  -DVTK_MODULE_ENABLE_ttkForEach=YES
-  -DVTK_MODULE_ENABLE_ttkEndFor=YES
-  -DVTK_MODULE_ENABLE_ttkGridLayout=YES
-  -DVTK_MODULE_ENABLE_ttkIcosphere=YES
-  -DVTK_MODULE_ENABLE_ttkIcosphereFromObject=YES
-  -DVTK_MODULE_ENABLE_ttkIcospheresFromPoints=YES
-  -DVTK_MODULE_ENABLE_ttkIdentifierRandomizer=YES
-  -DVTK_MODULE_ENABLE_ttkPointDataSelector=YES
-  -DVTK_MODULE_ENABLE_ttkScalarFieldNormalizer=YES
-  -DVTK_MODULE_ENABLE_ttkScalarFieldSmoother=YES
-  -DVTK_MODULE_ENABLE_ttkTextureMapFromField=YES
-)
-
-# topology
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkContinuousScatterPlot=YES
-  -DVTK_MODULE_ENABLE_ttkContourAroundPoint=YES
-  -DVTK_MODULE_ENABLE_ttkContourTreeAlignment=YES
-  -DVTK_MODULE_ENABLE_ttkFTMTree=YES
-  -DVTK_MODULE_ENABLE_ttkFTRGraph=YES
-  -DVTK_MODULE_ENABLE_ttkFiber=YES
-  -DVTK_MODULE_ENABLE_ttkFiberSurface=YES
-  -DVTK_MODULE_ENABLE_ttkPersistenceCurve=YES
-  -DVTK_MODULE_ENABLE_ttkPersistenceDiagram=YES
-  -DVTK_MODULE_ENABLE_ttkIntegralLines=YES
-  -DVTK_MODULE_ENABLE_ttkJacobiSet=YES
-  -DVTK_MODULE_ENABLE_ttkMeshGraph=YES
-  -DVTK_MODULE_ENABLE_ttkMorseSmaleComplex=YES
-  -DVTK_MODULE_ENABLE_ttkPeriodicGrid=YES
-  -DVTK_MODULE_ENABLE_ttkPlanarGraphLayout=YES
-  -DVTK_MODULE_ENABLE_ttkReebSpace=YES
-  -DVTK_MODULE_ENABLE_ttkScalarFieldCriticalPoints=YES
-  -DVTK_MODULE_ENABLE_ttkTopologicalSimplification=YES
-  -DVTK_MODULE_ENABLE_ttkTrackingFromFields=YES
-  -DVTK_MODULE_ENABLE_ttkTrackingFromOverlap=YES
-  -DVTK_MODULE_ENABLE_ttkTrackingFromPersistenceDiagrams=YES
-)
-
-# geometry processing
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkDepthImageBasedApproximation=YES
-  -DVTK_MODULE_ENABLE_ttkDistanceField=YES
-  -DVTK_MODULE_ENABLE_ttkGeometrySmoother=YES
-  -DVTK_MODULE_ENABLE_ttkManifoldCheck=YES
-  -DVTK_MODULE_ENABLE_ttkMorseSmaleQuadrangulation=YES
-  -DVTK_MODULE_ENABLE_ttkProjectionFromField=YES
-  -DVTK_MODULE_ENABLE_ttkQuadrangulationSubdivision=YES
-)
 if (eigen_enabled)
-  list(APPEND TTK_CORE_FILTER_LIST
-    -DVTK_MODULE_ENABLE_ttkEigenField=YES
-    -DVTK_MODULE_ENABLE_ttkHarmonicField=YES
+  list(APPEND ttk_enabled_modules
+    ttkEigenField
+    ttkHarmonicField
   )
 endif()
 
-# clustering
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkPersistenceDiagramClustering=YES
-)
-
-# ensemble
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkMandatoryCriticalPoints=YES
-  -DVTK_MODULE_ENABLE_ttkDimensionReduction=YES
-)
-
-# cinema
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkCinemaReader=YES
-  -DVTK_MODULE_ENABLE_ttkCinemaWriter=YES
-  -DVTK_MODULE_ENABLE_ttkCinemaQuery=YES
-  -DVTK_MODULE_ENABLE_ttkCinemaImaging=YES
-  -DVTK_MODULE_ENABLE_ttkCinemaProductReader=YES
-  -DVTK_MODULE_ENABLE_ttkCinemaDarkroom=YES
-)
-
-# compression
-list(APPEND TTK_CORE_FILTER_LIST
-  -DVTK_MODULE_ENABLE_ttkTopologicalCompressionReader=YES
-  -DVTK_MODULE_ENABLE_ttkTopologicalCompressionWriter=YES
-)
-
 # user defined modules
-foreach (ttk_module IN LISTS ttk_CUSTOM_FILTERS)
-  list(APPEND TTK_CORE_FILTER_LIST
+set(ttk_module_settings)
+foreach (ttk_module IN LISTS ttk_enabled_modules ttk_CUSTOM_FILTERS)
+  list(APPEND ttk_module_settings
     "-DVTK_MODULE_ENABLE_${ttk_module}=YES")
 endforeach ()
 
@@ -146,5 +130,5 @@ superbuild_add_project(ttk
     -DTTK_ENABLE_ZFP:BOOL=${zfp_enabled}
 
     -DTTK_WHITELIST_MODE:BOOL=TRUE
-    ${TTK_CORE_FILTER_LIST}
+    ${ttk_module_settings}
   )
