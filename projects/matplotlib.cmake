36390b915f0af2b9e619fcff4517c4b85701cca7
Ben Boeckel <ben.boeckel@kitware.com>
2021-08-29 16:07:31 -0400

    matplotlib: backport relevant bits from common-superbuild

--- projects/matplotlib.cmake
+++ projects/matplotlib.cmake
@@ -1,16 +1,9 @@
-set(matplotlib_args)
+set(matplotlib_process_environment)
 if (WIN32)
-  set(matplotlib_args
-    CONFIGURE_COMMAND
-      "${CMAKE_COMMAND}"
-      "-Dinstall_location=<INSTALL_DIR>"
-      "-Dskip_configure=TRUE"
-      -P "${CMAKE_CURRENT_LIST_DIR}/scripts/matplotlib.patch.cmake"
-
-    PROCESS_ENVIRONMENT
-      CL "/I<INSTALL_DIR>/include"
-      LINK "/LIBPATH:<INSTALL_DIR>/lib")
-endif()
+  list(APPEND matplotlib_process_environment
+    CL "/I<INSTALL_DIR>/include /I<INSTALL_DIR>/include/freetype2"
+    LINK "/LIBPATH:<INSTALL_DIR>/lib")
+endif ()
 
 set(matplotlib_depends)
 if (APPLE)
@@ -27,9 +20,14 @@ superbuild_add_project_python(matplotlib
   PACKAGE matplotlib
   DEPENDS numpy png freetype zlib pythondateutil pytz pythonpyparsing pythoncycler pythonsetuptools cxx11 pythonkiwisolver
           ${matplotlib_depends}
+  CONFIGURE_COMMAND
+    "${CMAKE_COMMAND}"
+    "-Dinstall_location=<INSTALL_DIR>"
+    "-Dpatches_location=${CMAKE_CURRENT_LIST_DIR}/patches"
+    "-Dsource_location:PATH=<SOURCE_DIR>"
+    -P "${CMAKE_CURRENT_LIST_DIR}/scripts/matplotlib.patch.cmake"
   PROCESS_ENVIRONMENT
-    "${matplotlib_process_environment}"
-  ${matplotlib_args})
+    ${matplotlib_process_environment})
 superbuild_apply_patch(matplotlib nostatic
   "Disable static builds")
 superbuild_apply_patch(matplotlib no-jquery
