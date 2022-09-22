617160a16193d57eff2c2111e7cb099532150f28
Ben Boeckel <ben.boeckel@kitware.com>
2022-09-22 16:51:43 -0400

    ci: enable scipy and surfacetrackercut on macOS arm64

--- .gitlab/ci/configure_macos.cmake
+++ .gitlab/ci/configure_macos.cmake
@@ -3,4 +3,7 @@ set(ENABLE_openmp OFF CACHE BOOL "")
 set(ENABLE_lookingglass ON CACHE BOOL "")
 set(ENABLE_threedxwaresdk ON CACHE BOOL "")
 
+# Add the rpath for fortran libraries.
+set(_superbuild_fortran_ld_flags "-Wl,-rpath,$ENV{CI_PROJECT_DIR}/.gitlab/gfortran/lib" CACHE STRING "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
