21abb83b9c233313b7de37d657765fd202e33466
Ben Boeckel <ben.boeckel@kitware.com>
2022-09-28 15:26:34 -0400

    ci: also pass `-L` for the gfortran libraries
    For some reason, the arm64 linker doesn't use `LC_RPATH` commands when
    linking libraries transitively. Just shove the link path in the search
    path so that `libgcc_s.dylib` ends up getting found.

--- .gitlab/ci/configure_macos.cmake
+++ .gitlab/ci/configure_macos.cmake
@@ -4,6 +4,6 @@ set(ENABLE_lookingglass ON CACHE BOOL "")
 set(ENABLE_threedxwaresdk ON CACHE BOOL "")
 
 # Add the rpath for fortran libraries.
-set(_superbuild_fortran_ld_flags "-Wl,-rpath,$ENV{CI_PROJECT_DIR}/.gitlab/gfortran/lib" CACHE STRING "")
+set(_superbuild_fortran_ld_flags "-Wl,-rpath,$ENV{CI_PROJECT_DIR}/.gitlab/gfortran/lib -L$ENV{CI_PROJECT_DIR}/.gitlab/gfortran/lib" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
