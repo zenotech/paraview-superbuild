ddf01e312b9a14e6570441d2ca150e5919f7f246
Ben Boeckel <ben.boeckel@kitware.com>
2023-09-04 12:19:18 -0400

    ci: use a static LLVM in OSMesa builds

--- .gitlab/ci/configure_linux_osmesa_shared.cmake
+++ .gitlab/ci/configure_linux_osmesa_shared.cmake
@@ -7,4 +7,7 @@ set(ENABLE_qt5                      OFF CACHE BOOL "")
 set(ENABLE_visrtx                   OFF CACHE BOOL "")
 set(ENABLE_vrpn                     OFF CACHE BOOL "")
 
+# Use a static LLVM to not ship extra LLVM libraries/code.
+set(llvm_BUILD_SHARED_LIBS        "OFF" CACHE STRING "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
