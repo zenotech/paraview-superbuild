467b59b25f56d1cb686769ccbe2786a3acc84a86
Ben Boeckel <ben.boeckel@kitware.com>
2023-09-08 07:21:26 -0400

    tests: update the scipy importing test

--- tests/python/import_scipy.py
+++ tests/python/import_scipy.py
@@ -2,4 +2,4 @@ import scipy
 
 # Ensure that the `arpack` directory does not interfere with the module. See
 # #223.
-import scipy.sparse.linalg.eigen.arpack.arpack
+import scipy.sparse.linalg._eigen.arpack.arpack
