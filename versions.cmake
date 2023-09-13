9b77112929bbd21b64d92e9c10b54315b0943b18
Ben Boeckel <ben.boeckel@kitware.com>
2023-09-13 13:59:28 -0400

    versions: fix vortexfinder2 link

--- versions.cmake
+++ versions.cmake
@@ -68,7 +68,7 @@ superbuild_set_revision(vrpn
   URL_MD5 3799426bc30d099587b2decfa9fb1f45)
 
 superbuild_set_revision(vortexfinder2
-  # https://github.com/hguo/vortexfinder2.git
+  # https://github.com/hguo/vortexfinder2
   URL     "https://www.paraview.org/files/dependencies/vortexfinder2-bb76f80ad08223d49fb42e828c1416daa19f7ecb.tar.bz2"
   URL_MD5 47d12a5103d66b5db782c43c5255b26b)
 
