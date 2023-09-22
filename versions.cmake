2c422a055590ac244d935dfea15510fd4e3cf37a
Ryan Krattiger <ryan.krattiger@kitware.com>
2023-09-22 10:22:28 -0500

    Update the default version to 5.11.2

--- versions.cmake
+++ versions.cmake
@@ -34,9 +34,9 @@ superbuild_set_revision(paraviewtutorialdata
 superbuild_set_selectable_source(paraview
   # NOTE: When updating this selection, also update the default version in
   # README.md and the PARAVIEW_VERSION_DEFAULT variable in CMakeLists.txt.
-  SELECT 5.11.1 DEFAULT
-    URL     "https://www.paraview.org/files/v5.11/ParaView-v5.11.1.tar.xz"
-    URL_MD5 57d87137fd02e3491ea4167c72cf7794
+  SELECT 5.11.2 DEFAULT
+    URL     "https://www.paraview.org/files/v5.11/ParaView-v5.11.2.tar.xz"
+    URL_MD5 b5a13247fec282b8294a28d3101049fb
   SELECT git CUSTOMIZABLE
     GIT_REPOSITORY "https://gitlab.kitware.com/paraview/paraview.git"
     GIT_TAG        "origin/master"
