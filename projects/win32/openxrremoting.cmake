350b7eaa15b6ef10a1463a687968b80013cf28d6
Ben Boeckel <ben.boeckel@kitware.com>
2023-11-14 11:43:03 -0500

    openxrremoting: rename the license file
    Its path is too long for WIX packaging, so rename it. The path in the
    package is indicative of its context.

--- projects/win32/openxrremoting.cmake
+++ projects/win32/openxrremoting.cmake
@@ -1,12 +1,17 @@
 # Make a project so that the user can enable/disable openxrremoting support
 superbuild_add_project(openxrremoting
   LICENSE_FILES
-    "Microsoft Holographic Remoting - Software Development Kit (SDK) Use Terms.txt"
+    "<BINARY_DIR>/SDK Terms.txt"
     "Third Party Notices.txt"
   CONFIGURE_COMMAND
     ""
   BUILD_COMMAND
-    ""
+    "${CMAKE_COMMAND}"
+      -Dsource_location:PATH=<SOURCE_DIR>
+      -Dbinary_location:PATH=<BINARY_DIR>
+      -P "${CMAKE_CURRENT_LIST_DIR}/scripts/openxrremoting.build.cmake"
+  BUILD_DEPENDS
+    "${CMAKE_CURRENT_LIST_DIR}/scripts/openxrremoting.build.cmake"
   INSTALL_COMMAND
     "${CMAKE_COMMAND}"
       -Dsource_location:PATH=<SOURCE_DIR>
