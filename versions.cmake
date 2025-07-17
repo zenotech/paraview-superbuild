3b5b5e828d942aaa7ad6dfa4e70461454a25e0bd
Cory Quammen <cory.quammen@kitware.com>
2025-07-17 10:39:49 +0000

    Update the default version to 6.0.0-RC3

--- versions.cmake
+++ versions.cmake
@@ -45,9 +45,9 @@ endif ()
 superbuild_set_selectable_source(paraview
   # NOTE: When updating this selection, also update the default version in
   # README.md and the PARAVIEW_VERSION_DEFAULT variable in CMakeLists.txt.
-  SELECT 6.0.0-RC2 ${paraview_release_default}
-    URL     "https://www.paraview.org/files/v6.0/ParaView-v6.0.0-RC2.tar.xz"
-    URL_MD5 d06f30965b3a0de13b22b50f37de222f
+  SELECT 6.0.0-RC3 ${paraview_release_default}
+    URL     "https://www.paraview.org/files/v6.0/ParaView-v6.0.0-RC3.tar.xz"
+    URL_MD5 fa5f685f9df79ee55c4862a6f613f635
   SELECT git CUSTOMIZABLE ${paraview_git_default}
     GIT_REPOSITORY "https://gitlab.kitware.com/paraview/paraview.git"
     GIT_TAG        "origin/master"
